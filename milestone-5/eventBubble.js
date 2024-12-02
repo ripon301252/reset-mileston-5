e2ebd6999cd65b5dbfcaea678d0e7f02576b41fa
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2024-12-02 17:23:11 +0600

    Add files via upload

---
+++ milestone-5/eventBubble.js
@@ -0,0 +1,16 @@
+document.getElementById('item-2').addEventListener('click', function(){
+    console.log('item-2 click')
+})
+
+document.getElementById('list-ul').addEventListener('click', function(){
+    console.log('ul click')
+})
+
+document.getElementById('list-container').addEventListener('click', function(){
+    console.log('section to click')
+})
+
+document.getElementById('body').addEventListener('click', function(){
+    console.log('main to click')
+})
+
